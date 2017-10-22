cbe2b5ddb7361cc06a3df3e82692db401af3043d
Krystian Jabłoński <Krystian Jaboski>
2017-10-22 23:21:18 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,44 @@
+    // let iPhone6s = Phone {
+    //     brand: Apple,
+    //     price: 360,
+    //     color: white,
+    //     isAvailable: false.
+    // }
+    // let SamsungS8 = Phone {
+    //     brand: Samsung,
+    //     price: 200,
+    //     color: black,
+    //     isAvailable: true.
+    // }
+    // let XiamomiMI = Phone {
+    //     brand: Xiaomi,
+    //     price: 10,
+    //     color: invisible,
+    //     isAvailable: false.
+    // }
+    // Czemu nie działają te wyżej?
+
+    let iPhone6s = new Phone('Apple', 360, 'white', 'false');
+    let SamsungS8 = new Phone('Samsung', 200, 'black', 'true');
+    let XiamomiMI = new Phone('Xiaomi', 10, 'invisible', 'false');
+
+    let isAvailable = function() {
+        if(isAvailable != false) {
+            console.log("Available")
+        }
+        else {
+            console.log("Not Available")
+        }
+    };
+    function Phone(brand, price, color){
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    };
+    Phone.prototype.println = function(){
+        console.log(`Phone brand is ${this.brand}, the color is ${this.color} and the price equals to ${this.price}$ and is ${isAvailable}`);
+    };
+
+iPhone6s.println();
+SamsungS8.println();
+XiamomiMI.println();
